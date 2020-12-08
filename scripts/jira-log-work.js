808727553288a5b2e289c6397deaeca2e02690f4
andrei.krasovskii <andrei.krasovskii@p2h.com>
2020-12-08 21:13:18 +0200

    Count all logs in jira-log-work

--- scripts/jira-log-work.js
+++ scripts/jira-log-work.js
@@ -2,7 +2,7 @@
 // @name         Jira show loged time
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to do nothing!
+// @description  do less get more!
 // @author       Krass
 // @match        https://jira.tech/browse/*
 // @grant        none
@@ -10,63 +10,132 @@
 // ==/UserScript==
 
 (function() {
-  	/*внести изменения*/
+	/*внести изменения*/
 
-    // mail по которому пользователь залоган в джире:
-	// let user = 'andrei.krasovskii@p2h.com';
-	let user = 'aleksandr.nektarov@p2h.com';
-
-    // месяц за который учитывать время, примеры: Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec
-	let moth = 'Sep';
+	// mail по которому пользователь залоган в джире:
+	let userMail = 'andrei.krasovskii@p2h.com';
+	// месяц за который учитывать время, примеры: Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec
+	let month = 'Dec';
+	// месяц за который учитывать время пример: 20
 	let year = '20';
 
 	/*дальше не трогать*/
+
 	let allLogs = document.querySelectorAll('.issue-data-block');
-	let userSelector = `[rel='${user}']`;
+
 	let currentUserLogs =[],
-		currentMothLogs = [],
-		time = 0;
-	if(!allLogs.length) {return};
-	allLogs.forEach((log) => {
-		if (log.querySelectorAll(userSelector).length){
-			currentUserLogs.push(log);
+		currentYearLogs = [],
+		currentMonthLogs = [],
+		monthTime,
+		allTime;
+
+	if(!allLogs.length) {return}
+
+	currentUserLogs = filterByUser(allLogs, userMail);
+
+	if(currentUserLogs.length) {return;}
+
+	allTime = countTime(currentUserLogs);
+
+	currentYearLogs = filterByYear(currentUserLogs, year);
+	currentMonthLogs = filterByMonth(currentYearLogs, month);
+	monthTime = countTime(currentMonthLogs);
+
+	addTime(allTime, monthTime);
+
+
+	function filterByUser(allLogs, userMail) {
+		let currentUserLogs = [];
+		let userSelector = `[rel='${userMail}']`;
+
+		allLogs.forEach((log) => {
+			if (log.querySelectorAll(userSelector).length){
+				currentUserLogs.push(log);
+			}
+		});
+
+		return currentUserLogs;
+	}
+
+	function filterByYear(currentUserLogs, year) {
+		let currentYearLogs = [];
+
+		currentUserLogs.forEach((log) => {
+			if (log.querySelector('.date').textContent.includes('/' + year)) {
+				currentYearLogs.push(log);
+			}
+		})
+
+		return currentYearLogs;
+	}
+
+	function filterByMonth(currentYearLogs, month) {
+		let currentMonthLogs = [];
+
+		currentUserLogs.forEach((log) => {
+			if (log.querySelector('.date').textContent.includes(month)) {
+				currentMonthLogs.push(log);
+			}
+
+		})
+
+		return currentMonthLogs;
+	}
+
+	function countTime(logs) {
+		let time = 0;
+		let hoursInDay = 6;
+		let minutesInHour = 60;
+
+		if (logs.length){
+			return time;
 		}
-	})
 
-	currentUserLogs.forEach((log) => {
-		if (log.querySelector('.date').textContent.includes(moth) && log.querySelector('.date').textContent.includes('/' + year)) {
-			currentMothLogs.push(log);
-		}
+		logs.forEach((log) => {
+			let strTime = log.querySelector('.worklog-duration').textContent;
+			let intDay = parseInt(/\d* day/.exec(strTime), 10) || 0;
+			let inthour = parseInt(/\d* hour/.exec(strTime), 10) || 0;
+			let intMin = parseInt(/\d* minutes/.exec(strTime), 10) || 0;
 
-	})
+			time += intDay * hoursInDay + inthour + intMin / minutesInHour;
+		})
 
-	currentMothLogs.forEach((log) => {
-		let strTime = log.querySelector('.worklog-duration').textContent;
-		let intDay = parseInt(/\d* day/.exec(strTime), 10) || 0;
-		let inthour = parseInt(/\d* hour/.exec(strTime), 10) || 0;
-		let intMin = parseInt(/\d* minutes/.exec(strTime), 10) || 0;
+		time = time % 1 === 0 ? time : time.toFixed(2);
 
-		time += intDay * 6 + inthour + intMin / 60;
-	})
+		return time;
+	}
 
-	let span = document.createElement('span');
-	let logTab = document.querySelector('#worklog-tabpanel');
+	function addTime(allTime, monthTime) {
+		let spanMonth = document.createElement('span');
+		let spanAll = document.createElement('span');
+		let logTab = document.querySelector('#worklog-tabpanel');
+		let commonStyle = `
+			position: absolute;
+			top: -12px;
+			border-radius: 5px;
+			font-size: 11px;
+			padding: 2px;
+			font-weight: 500;
+		`;
+		let monthStyle = `
+			right: 0;
+			background: #0747a6;
+			color: #fff;
+		`;
+		let allStyle = `
+			left: 0;
+			background: #ffd351;
+			color: #594300;
+		`;
 
-    time = time % 1 === 0 ? time : time.toFixed(2);
-	span.innerHTML = time;
-	span.style.cssText = `
-		position: absolute;
-		top: -12px;
-		right: 0;
-		background: #0747a6;
-		border-radius: 5px;
-		color: #fff;
-		font-size: 11px;
-		padding: 2px;
-		font-weight: 500;
-	`;
-	logTab.appendChild(span)
 
-	console.log(time + ' hour');
+		spanMonth.innerHTML = monthTime;
+		spanMonth.style.cssText = commonStyle + monthStyle;
+		logTab.appendChild(spanMonth)
+
+		spanAll.innerHTML = allTime;
+		spanAll.style.cssText = commonStyle + allStyle;
+		logTab.appendChild(spanAll)
+	}
 
 })();
